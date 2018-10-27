e71efda4682666fbf91de8bdfadf38b125ddfbc5
aflaverde <42219494+aflaverde@users.noreply.github.com>
2018-10-27 15:09:08 -0500

    Con transformada

--- Fourier.py
+++ Fourier.py
@@ -16,4 +16,17 @@ y_inc=incomp[:,1]
 #Grafica los datos de signal.dat y lo guarda en LaverdeAndres_signal.pdf
 plt.figure()
 plt.plot(x_sig, y_sig)
+plt.savefig("LaverdeAndres_signal.pdf")
+
+######FOURIER######
+n_sig=len(x_sig)
+fourier_sig=fft(y_sig)/n_sig #Funcion normalizada con Fourier
+
+dx_sig=x_sig[1]-x_sig[0]
+f_sig=fftfreq(n_sig, dx_sig) #Frecuencia de los datos
+
+plt.figure()
+plt.plot(f_sig, abs(fourier_sig))
+plt.xlabel("Frecuencia")
+plt.ylabel("Transformada de Fourier")
 plt.show()

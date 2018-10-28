781f909c55c6f2efe245d46099aaeb819c4fd637
aflaverde <42219494+aflaverde@users.noreply.github.com>
2018-10-28 15:29:10 -0500

    Con diagonal de matriz de covarianza
    Falta el resto de la matriz

--- PCR.py
+++ PCR.py
@@ -1,16 +1,38 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 arch=np.genfromtxt("WDBC.dat", delimiter=',')
 letras=arch[:,1]
 arch=np.delete(arch,1,1)	#Elimina la columna con letras
 arch=np.delete(arch,0,1)	#Elimina la primera columna del ID
 
-print(arch.shape)
+n=arch.shape[0]
 
-for j in range(arch.shape[1]):
-	col_j=arch[:,j]		#Guarda cada columna como variable
-	mean_j=np.mean(col_j) #Halla el promedio de cada columna
-	col_j-=mean_j		#Resta a cada columna el promedio de esa columna
-	#print(sum(col_j))	Para comprobar que esta correctamente hecho se suman todos los elementos de cada columna y deberia dar 0
-
-print(arch)
+def varianza_diagonal(ar,columna):
+	suma=0
+	for i in range(ar.shape[0]):
+		suma+=(ar[i,columna]-np.mean(ar[:,columna]))**2
+	varianza=suma/n
+	return varianza
+	#Resta a cada elemento el promedio de su columna (al cuadrado) y lo divide por el numero de datos para la varianza de cada variable. Esto lo aniade a la diagonal de la matriz de covarianza.	
+	
+def covarianza_simetrica(ar,columna):
+	suma=0
+	for i in range(ar.shape[0]):
+		suma+=(ar[i,columna]-np.mean(ar[:,columna]))*(ar[i,columna+1]-np.mean(ar[:,columna+1]))
+	varianza=suma/n
+	
+def covarianza(ar):
+	covar=np.zeros((ar.shape[1],ar.shape[1]))
+	#inicializa la matriz de covarianza en ceros con una forma m x m donde m es el numero de variables
+	
+	###Metodo de covarianza###
+	for i in range(covar.shape[0]):
+		for j in range(covar.shape[0]):
+			if i==j:
+				covar[i][j]=varianza_diagonal(ar,j)
+			else:
+				covar[i][j]=covarianza_simetrica(ar,j)
+	return covar
+	
+covar=covarianza(arch)

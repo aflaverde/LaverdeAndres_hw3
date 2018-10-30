0dbdac38b98568afb3582f7cbfb03102d4a44d80
aflaverde <42219494+aflaverde@users.noreply.github.com>
2018-10-29 22:32:47 -0500

    No completo
    Hay un problema con el contador para la matriz de covarianza, para los datos que no son de la diagonal

--- PCR.py
+++ PCR.py
@@ -16,10 +16,10 @@ def varianza_diagonal(ar,columna):
 	return varianza
 	#Resta a cada elemento el promedio de su columna (al cuadrado) y lo divide por el numero de datos para la varianza de cada variable. Esto lo aniade a la diagonal de la matriz de covarianza.	
 	
-def covarianza_simetrica(ar,columna):
+def covarianza_simetrica(ar, j, contador_columna):
 	suma=0
 	for i in range(ar.shape[0]):
-		suma+=(ar[i,columna]-np.mean(ar[:,columna]))*(ar[i,columna+1]-np.mean(ar[:,columna+1]))
+		suma+=(ar[i,j]-np.mean(ar[:,j]))*(ar[i,contador_columna]-np.mean(ar[:,contador_columna]))
 	covarianza=suma/n
 	return covarianza
 	#Retorna la covarianza de las partes inferior y superior de la diagonal de la matriz
@@ -27,14 +27,16 @@ def covarianza_simetrica(ar,columna):
 def covarianza(ar):
 	covar=np.zeros((ar.shape[1],ar.shape[1]))
 	#inicializa la matriz de covarianza en ceros con una forma m x m donde m es el numero de variables
-	
+	contador_columna=0 #Este contador cambia la posicion del "pivote" cada vez que hace una iteracion
 	###Metodo de covarianza###
 	for i in range(covar.shape[0]):
-		for j in range(covar.shape[0]):
+		for j in range(ar.shape[1]):
 			if i==j:
 				covar[i][j]=varianza_diagonal(ar,j)
-			else:
-				covar[i][j]=covarianza_simetrica(ar,j)
+			elif i!=j:
+				covar[i][j]=covarianza_simetrica(ar,j,contador_columna)
+			contador_columna+=1
 	return covar
 	
 covar=covarianza(arch)
+print(covar)

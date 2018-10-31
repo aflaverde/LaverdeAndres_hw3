8e0995f5bf142ba79f4e871b1f308069c5074da5
aflaverde <42219494+aflaverde@users.noreply.github.com>
2018-10-30 21:09:08 -0500

    PCR con matriz de covarianza
    ademas tiene los eigen values y vectors

--- PCR.py
+++ PCR.py
@@ -2,41 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 arch=np.genfromtxt("WDBC.dat", delimiter=',')
-letras=arch[:,1]
+ID=arch[:,0]
+tipo=arch[:,1]
 arch=np.delete(arch,1,1)	#Elimina la columna con letras
 arch=np.delete(arch,0,1)	#Elimina la primera columna del ID
 
-n=arch.shape[0]
-
-def varianza_diagonal(ar,columna):
-	suma=0
-	for i in range(ar.shape[0]):
-		suma+=(ar[i,columna]-np.mean(ar[:,columna]))**2
-	varianza=suma/n
-	return varianza
-	#Resta a cada elemento el promedio de su columna (al cuadrado) y lo divide por el numero de datos para la varianza de cada variable. Esto lo aniade a la diagonal de la matriz de covarianza.	
-	
-def covarianza_simetrica(ar, j, contador_columna):
-	suma=0
-	for i in range(ar.shape[0]):
-		suma+=(ar[i,j]-np.mean(ar[:,j]))*(ar[i,contador_columna]-np.mean(ar[:,contador_columna]))
-	covarianza=suma/n
-	return covarianza
-	#Retorna la covarianza de las partes inferior y superior de la diagonal de la matriz
-	
-def covarianza(ar):
-	covar=np.zeros((ar.shape[1],ar.shape[1]))
-	#inicializa la matriz de covarianza en ceros con una forma m x m donde m es el numero de variables
-	contador_columna=0 #Este contador cambia la posicion del "pivote" cada vez que hace una iteracion
-	###Metodo de covarianza###
-	for i in range(covar.shape[0]):
-		for j in range(ar.shape[1]):
-			if i==j:
-				covar[i][j]=varianza_diagonal(ar,j)
-			elif i!=j:
-				covar[i][j]=covarianza_simetrica(ar,j,contador_columna)
-			contador_columna+=1
+def covarianza(data):
+	var=data.shape[1]
+	n=float(data.shape[0])
+	covar=np.zeros((var,var)) #inicializa la matriz de covarianza en ceros con una forma m x m donde m es el numero de variables 
+	for i in range(var):
+		for j in range(var):
+			mean_var1=np.mean(data[:,i])
+			mean_var2=np.mean(data[:,j])
+			covar[i,j]=np.sum((data[:,i]-mean_var1)*(data[:,j]-mean_var2))/n
+			#Resta a cada elemento el promedio de su columna multiplicando por lo mismo o por la siguiente variable sea el caso, suma todo lo anterior y luego lo divide por el numero de datos para la varianza de cada variable.
 	return covar
-	
+			
 covar=covarianza(arch)
-print(covar)
+
+eig_val=np.linalg.eig(covar)[0]
+eig_vec=np.linalg.eig(covar)[1]
+
+print("--------------EIGENVALUES Y EIGENVECTORS------------")
+for i in range(len(eig_val)):
+	print("Eigenvalue",i,":",eig_val[i]," Respectivo Eigenvector:", eig_vec[i])
+	
+print("-->")
+
+######Proyeccion
